5db719b2d727bb3075feb5fa555e882393763874
Caio Andrade Mota <cam.snf19@uea.edu.br>
2019-11-25 07:46:23 -0400

    uri1008

---
+++ 1008.py
@@ -0,0 +1,8 @@
+number = int(input())
+worktime = int(input())
+dolar_per_hour = float(input())
+
+salary = worktime * dolar_per_hour
+
+print("NUMBER =", number)
+print("SALARY = US$ %.2f" % salary)
