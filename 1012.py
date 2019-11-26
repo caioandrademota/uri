bae357242b93f0e787d953252425c5f132ff4333
Caio Andrade Mota <cam.snf19@uea.edu.br>
2019-11-26 07:24:13 -0400

    uri 1012

---
+++ 1012.py
@@ -0,0 +1,16 @@
+a, b, c = input().split(' ')
+a = float(a)
+b = float(b)
+c = float(c)
+
+triangle = a * c/2
+circle = 3.14159 * (c**2)
+trapezium = (c * (a + b))/2
+square = b**2
+rectangle = a * b
+
+print("TRIANGULO: %.3f" %triangle)
+print("CIRCULO: %.3f" %circle)
+print("TRAPEZIO: %.3f" %trapezium)
+print("QUADRADO: %.3f" %square)
+print("RETANGULO: %.3f" %rectangle)
