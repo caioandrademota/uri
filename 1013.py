db21a134cf79bba6636abf817e0e77c15ae8e08b
Caio Andrade Mota <cam.snf19@uea.edu.br>
2019-11-26 07:33:06 -0400

    uri 1013

---
+++ 1013.py
@@ -0,0 +1,10 @@
+a, b, c = input().split(' ')
+a = int(a)
+b = int(b)
+c = int(c)
+
+
+maior1 = (a + b + abs(a - b))/2
+maior2 = (b + c + abs(b - c))/2
+maior = (maior1 + maior2 + abs(maior1 - maior2))/2
+print("%i eh o maior" %maior)
