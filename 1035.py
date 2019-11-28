4a79b75375e82a809e56c04d6679fe053753d678
Caio Andrade Mota <cam.snf19@uea.edu.br>
2019-11-28 08:23:04 -0400

    uri1035

---
+++ 1035.py
@@ -0,0 +1,19 @@
+a, b, c, d = input().split(' ')
+a = int(a)
+b = int(b)
+c = int(c)
+d = int(d)
+
+if b > c and d > a:
+    if c + d > a + b:
+        if c > 0 and d > 0:
+            if a % 2 == 0:
+                print("Valores aceitos")
+            else:
+                print("Valores nao aceitos")
+        else:
+            print("Valores nao aceitos")
+    else:
+        print("Valores nao aceitos")
+else:
+    print("Valores nao aceitos")
