ea8b3da8c55b0be8f9dcb54d667b136d816ec640
Caio Andrade Mota <cam.snf19@uea.edu.br>
2019-12-06 00:07:40 -0400

    uri 1040 update

--- 1040.py
+++ 1040.py
@@ -1,9 +1,12 @@
 def exame(media):
+    print("Media: %.1f" % media)
     print("Aluno em exame.")
-    exame = float(input("Nota do exame: "))
+    exame = float(input())
+    print("Nota do exame: ", exame)
     mf = (media + exame)/2
     if mf >= 5.0:
         print("Aluno aprovado.")
+        print("Media final: %.1f" % mf)
     else:
         print("Aluno reprovado.")
 
@@ -18,9 +21,10 @@ n4 = n4
 media = (n1 + n2 + n3 + n4)/10
 
 if media >= 7.0:
-    print("Media:", media)
+    print("Media: %.1f" % media)
     print("Aluno aprovado.")
-elif media >= 5.0 and media < 7.0:
+elif media >= 5.0 and media <= 6.9:
     exame(media)
 else:
+    print("Media: %.1f" % media)
     print("Aluno reprovado.")
